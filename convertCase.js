201230b8583653d0318fe07adb0b77c4a3723ffa
dj_yoko <yokochan.recurrent+webdev@gmail.com>
2018-09-29 00:52:27 +0900

    rename JS

---
+++ convertCase.js
@@ -0,0 +1,9 @@
+ /**
+  * method
+  * @param hoge {Object} - hoge Object
+  */
+ const convertCase = function (input, type) {
+ 	if (typeof input !== 'string' || typeof type !== 'string') {
+ 		return false;
+ 	}
+ }
