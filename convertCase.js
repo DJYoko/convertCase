bf0ad5b00fd4de0a6f28fc048e4b6ed3df484013
dj_yoko <yokochan.recurrent+webdev@gmail.com>
2018-09-29 01:06:08 +0900

    create switch case for exchange type (each metod is not developped)

--- convertCase.js
+++ convertCase.js
@@ -1,9 +1,34 @@
  /**
   * method
-  * @param hoge {Object} - hoge Object
+  * @param input {string} - input string
+  * @param type  {string} - type of exchange (cs,ck,sc,sk,ks,kc)
   */
  const convertCase = function (input, type) {
- 	if (typeof input !== 'string' || typeof type !== 'string') {
+ 	const exchange_type_set = ['ck', 'cs', 'kc', 'ks', 'sc', 'sk'];
+
+ 	if (typeof input !== 'string' || typeof type !== 'string' || exchange_type_set.indexOf(type) === -1) {
  		return false;
  	}
+
+ 	switch (exchange_type_set.indexOf(type)) {
+ 		case 0:
+ 			// camelCase to kebab-case
+ 			break;
+ 		case 1:
+ 			// camelCase to snake_case 
+ 			break;
+ 		case 2:
+ 			// kebab-case to camelCase
+ 			break;
+ 		case 3:
+ 			// kebab-case to snake_case
+ 			break;
+ 		case 4:
+ 			// kebab-case to snake_case
+ 			break;
+ 		case 5:
+ 			// snake_case to kebab-case
+ 			break;
+ 	}
+
  }
