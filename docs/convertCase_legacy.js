012bba3c4c4854c2392b2973e010373065012601
dj_yoko <yokochan.recurrent@gmail.com>
2020-05-01 23:31:51 +0900

    Rename option [Constructor => Pascal]

--- docs/convertCase_legacy.js
+++ docs/convertCase_legacy.js
@@ -1,55 +1,57 @@
- /**
-  * method
-  * @param params {object}
-  * @param params.input {string} - input string
-  * @param params.type  {string} - type of exchange (cs,ck,sc,sk,ks,kc)
-  * @param params.isConstructor  {boolean} - is Constructor (Capitalize the 1st letter)
-  */
+/**
+ * method
+ * @param params {object}
+ * @param params.input {string} - input string
+ * @param params.type  {string} - type of exchange (cs,ck,sc,sk,ks,kc)
+ * @param params.isPascal  {boolean} - is Pascal (Capitalize the 1st letter)
+ */
 
- const convertCase = function (params) {
+const convertCase = function (params) {
+  let return_string = ''
 
-   let return_string = '';
+  const REGEX_LIST = {
+    ck: function (arg) {
+      // camelCase to kebab-case
+      return arg.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
+    },
+    cs: function (arg) {
+      // camelCase to snake_case
+      return arg.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase()
+    },
+    kc: function (arg) {
+      // kebab-case to camelCase
+      const replacer = function (match) {
+        return match.replace(/\-/g, '').toUpperCase()
+      }
+      return arg.replace(/\-[a-z]/g, replacer)
+    },
+    ks: function (arg) {
+      // kebab-case to snake_case
+      return arg.replace(/\-/g, '_')
+    },
+    sc: function (arg) {
+      // snake_case to camelCase
+      const replacer = function (match) {
+        return match.replace(/\_/g, '').toUpperCase()
+      }
+      return arg.replace(/\_[a-z]/g, replacer)
+    },
+    sk: function (arg) {
+      // snake_case to kebab-case
+      return arg.replace(/_/g, '-')
+    },
+  }
 
-   const REGEX_LIST = {
-     'ck': function (arg) {
-       // camelCase to kebab-case
-       return arg.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
-     },
-     'cs': function (arg) {
-       // camelCase to snake_case 
-       return arg.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase();
-     },
-     'kc': function (arg) {
-       // kebab-case to camelCase
-       const replacer = function (match) {
-         return match.replace(/\-/g, '').toUpperCase();
-       }
-       return arg.replace(/\-[a-z]/g, replacer);
-     },
-     'ks': function (arg) {
-       // kebab-case to snake_case
-       return arg.replace(/\-/g, '_');
-     },
-     'sc': function (arg) {
-       // snake_case to camelCase
-       const replacer = function (match) {
-         return match.replace(/\_/g, '').toUpperCase();
-       }
-       return arg.replace(/\_[a-z]/g, replacer);
-     },
-     'sk': function (arg) {
-       // snake_case to kebab-case
-       return arg.replace(/_/g, '-');
-     },
-   };
+  return_string = REGEX_LIST[params.type](params.input)
 
-   return_string = REGEX_LIST[params.type](params.input);
+  // Pascal option
+  if (
+    params.isPascal === true &&
+    (params.type === 'kc' || params.type === 'sc')
+  ) {
+    return_string =
+      return_string.charAt(0).toUpperCase() + return_string.slice(1)
+  }
 
-   // constructor option
-   if (params.isConstructor === true && (params.type === 'kc' || params.type === 'sc')) {
-     return_string = return_string.charAt(0).toUpperCase() + return_string.slice(1);
-   }
-
-   return return_string;
-
- }
+  return return_string
+}
--- src/convertCase.ts
+++ src/convertCase.ts
@@ -11,13 +11,13 @@ import REGEX_LIST from './REGEX_LIST';
  * @param params {object}
  * @param params.input {string} - input string
  * @param params.type  {string} - type of exchange (cs,ck,sc,sk,ks,kc)
- * @param params.isConstructor  {boolean} - is Constructor (Capitalize the 1st letter)
+ * @param params.isPascal  {boolean} - is Pascal (Capitalize the 1st letter)
  */
 window.convertCase = (params: any) => {
 
     let return_string = REGEX_LIST[params.type](params.input);
-    // isConstructor option
-    if (params.isConstructor === true && (params.type === 'kc' || params.type === 'sc')) {
+    // isPascal option
+    if (params.isPascal === true && (params.type === 'kc' || params.type === 'sc')) {
         return_string = return_string.charAt(0).toUpperCase() + return_string.slice(1);
     }
 
